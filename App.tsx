87cadc20d07a7b1d75eb2498723879baf60a71e7
Hildebrando Viana Matos <hildocontato@gmail.com>
2022-06-05 16:54:07 -0300

    Using fonts on React Native

--- App.tsx
+++ App.tsx
@@ -1,12 +1,32 @@
 import React from "react";
 
+import AppLoading from "expo-app-loading";
+
 import { ThemeProvider } from "styled-components";
+import theme from "./src/global/styles/theme";
 
 import { Dashboard } from "./src/screens/Dashboard";
 
-import theme from "./src/global/styles/theme";
+// Fonts
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  // If fonts it's loading, the user will stay on Splash Screen
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
