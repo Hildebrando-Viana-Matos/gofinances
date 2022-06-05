92485edede4bfb7cdd89d22fd99fd55c6c328861
Hildebrando Viana Matos <hildocontato@gmail.com>
2022-06-05 17:31:41 -0300

    Usign react-native-responsive-fontsize

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,32 @@
 import React from "react";
 
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+} from "./styles";
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{ uri: "https://github.com/Hildebrando-Viana-Matos.png" }}
+            />
+            <User>
+              <UserGreeting>Olá, </UserGreeting>
+              <UserName>Hildebrando</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
