a16e8658cd9bdc94173fdd0690435c1869420009
Hildebrando Viana Matos <hildocontato@gmail.com>
2022-06-10 16:14:27 -0300

    Using Forms and Reack Hook Forms on React Native

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -5,15 +5,15 @@ export default {
     secondary_light: "rgba(255, 135, 44, 0.3)",
 
     success: "#12a454",
-    success_light: "rgba(18, 164, 84, 0.5)",
+    success_light: "rgba(18, 164, 84, 0.1)",
 
     attention: "#e83f5b",
-    attention_light: "rgba(232, 63, 91, 0.5)",
+    attention_light: "rgba(232, 63, 91, 0.1)",
 
     shape: "#FFFFFF",
     title: "#363f5f",
     text: "#969cb2",
-    text_dark: "#000",
+    text_dark: "#000000",
     background: "#f0f2f5",
   },
 
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,11 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { Modal } from "react-native";
+
+import { useForm } from "react-hook-form";
+
 import { Button } from "../../components/Form/Button";
-
 import { Input } from "../../components/Form/Input";
+import { InputForm } from "../../components/Form/InputForm";
+import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
+import { CategorySelectButton } from "../../components/Form/CategorySelectButton";
 
-import { Container, Header, Title, Form, Fields } from "./styles";
+import { CategorySelect } from "../CategorySelect";
+
+import {
+  Container,
+  Header,
+  Title,
+  Form,
+  Fields,
+  TransactionTypes,
+} from "./styles";
+
+interface FormData {
+  [name: string]: string;
+}
 
 export function Register() {
+  const [transactionType, setTransactionType] = useState("");
+  const [categoryModalOpen, setCategoryModalOpen] = useState(false);
+  const [category, setCategory] = useState({
+    key: "category",
+    name: "Categoria",
+  });
+
+  const { control, handleSubmit } = useForm();
+
+  function handleTransactionTypeSelect(type: "up" | "down") {
+    setTransactionType(type);
+  }
+
+  function handleOpenSelectCategoryModal() {
+    setCategoryModalOpen(true);
+  }
+
+  function handleCloseSelectCategoryModal() {
+    setCategoryModalOpen(false);
+  }
+
+  function handleRegister(form: FormData) {
+    const data = {
+      name: form.name,
+      amount: form.amount,
+      transactionType,
+      category: category.key,
+    };
+
+    console.log(data);
+  }
+
   return (
     <Container>
       <Header>
@@ -14,11 +66,39 @@ export function Register() {
 
       <Form>
         <Fields>
-          <Input placeholder="Nome" />
-          <Input placeholder="Preço" />
+          <InputForm name="name" control={control} placeholder="Nome" />
+          <InputForm name="amount" control={control} placeholder="Preço" />
+
+          <TransactionTypes>
+            <TransactionTypeButton
+              type="up"
+              title="Income"
+              onPress={() => handleTransactionTypeSelect("up")}
+              isActive={transactionType === "up"}
+            />
+            <TransactionTypeButton
+              type="down"
+              title="Outcome"
+              onPress={() => handleTransactionTypeSelect("down")}
+              isActive={transactionType === "down"}
+            />
+          </TransactionTypes>
+
+          <CategorySelectButton
+            title={category.name}
+            onPress={handleOpenSelectCategoryModal}
+          />
         </Fields>
-        <Button title="Enviar" />
+        <Button title="Enviar" onPress={handleSubmit(handleRegister)} />
       </Form>
+
+      <Modal visible={categoryModalOpen}>
+        <CategorySelect
+          category={category}
+          setCategory={setCategory}
+          closeSelectCategory={handleCloseSelectCategoryModal}
+        />
+      </Modal>
     </Container>
   );
 }
