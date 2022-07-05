c2c770f6698a36cd5f431acaae5998e3937f41de
Hildebrando Viana Matos <hildocontato@gmail.com>
2022-07-05 18:59:33 -0300

    Using env variables

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,6 +1,9 @@
-import React, { createContext, ReactNode, useContext } from "react";
+import React, { createContext, ReactNode, useContext, useState } from "react";
 
 import * as AuthSession from "expo-auth-session";
+
+const { CLIENT_ID } = process.env;
+const { REDIRECT_URI } = process.env;
 
 interface AuthProviderProps {
   children: ReactNode;
@@ -18,27 +21,41 @@ interface AuthContextData {
   signInWithGoogle(): Promise<void>;
 }
 
+interface AuthorizationResponse {
+  params: {
+    access_token: string;
+  };
+  type: string;
+}
+
 const AuthContext = createContext({} as AuthContextData);
 
 function AuthProvider({ children }: AuthProviderProps) {
-  const user = {
-    id: "iahfdoanodnfoainf",
-    name: "Hildebrando Viana Matos",
-    email: "hildocontato@gmail.com",
-  };
+  const [user, setUser] = useState<User>({} as User);
 
   async function signInWithGoogle() {
     try {
-      const CLIENT_ID =
-        "937226741771-lk41mudlafi85karmtjkcndatk37vvk6.apps.googleusercontent.com";
-      const REDIRECT_URI = "https://auth.expo.io/@hildomatos/gofinances";
       const RESPONSE_TYPE = "token";
       const SCOPE = encodeURI("profile email");
 
       const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
 
-      const response = await AuthSession.startAsync({ authUrl });
-      console.log(response);
+      const { type, params } = (await AuthSession.startAsync({
+        authUrl,
+      })) as AuthorizationResponse;
+      if (type === "success") {
+        const response = await fetch(
+          `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`
+        );
+        const userInfo = await response.json();
+
+        setUser({
+          id: userInfo.id,
+          email: userInfo.email,
+          name: userInfo.given_name,
+          photo: userInfo.picture,
+        });
+      }
     } catch (error: any) {
       throw new Error(error);
     }
